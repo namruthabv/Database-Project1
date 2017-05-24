e9548e2f2b5ca3e3771baa769d4dc0802552c2ca
namruthabv <namruthabv12@gmail.com>
2017-05-24 13:28:07 -0500

    connection file credentials changed

--- connect.php
+++ connect.php
@@ -1,14 +1,14 @@
 <?php
 $servername = "dcm.uhcl.edu";
-$username = "bogimanevijayakn";
-$password = "1546184";
+$username = "username";
+$password = "password";
 $dbname = "sakila";
 // connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if(!$conn){
-	 printf("Can't connect to MySQL Server. Errorcode: %s\n", 
+	 printf("Can't connect to MySQL Server. Errorcode: %s\n",
        mysqli_connect_error());
    exit;
-}	
-	
+}
+
 ?>
